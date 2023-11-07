26b285db757a3177312230524c3e380923911d81
Timur-Gus <timurka24478@gmail.ru>
2023-11-07 20:16:39 +0300

    Initial commit

---
+++ src/Book.java
@@ -0,0 +1,29 @@
+public class Book {
+    private int yearPublication;
+    private String nameBook;
+    private Author author;
+    public Book(int yearPublication,String nameBook,Author author){
+        this.author = author;
+        this.nameBook = nameBook;
+        this.yearPublication = yearPublication;
+    }
+    public int getYearPublication(){
+        return yearPublication;
+    }
+    public String getNameBook(){
+        return nameBook;
+    }
+    public Author getAuthor(){
+        return author;
+    }
+    public void setYearPublication(int yearPublication){
+        this.yearPublication = yearPublication;
+    }
+    public void setNameBook(String nameBook){
+        this.nameBook = nameBook;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+}
